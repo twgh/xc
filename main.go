1981b1955182802ab0e7782e345fe31475adf048
twgh <twgh521@qq.com>
2025-10-16 23:00:34 +0800

    update to v0.0.2

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 		Use:   "version",
 		Short: "显示版本信息",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("xc version 0.0.1")
+			fmt.Println("xc version 0.0.2")
 		},
 	}
 
